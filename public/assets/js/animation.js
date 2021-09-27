b31d0db625727abd5948939e55336c5ff6d49972
ccardinale98 <ccardinale98@gmail.com>
2021-09-27 18:16:45 -0400

    added letter animation

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -2,7 +2,16 @@ document.getElementById("show-hide").style.display = "none";
 
 $('#cover').on('click', function () {
     hideCover();
-    showContent();    
+    showContent();
+    
+    anime.timeline({loop: false})
+    .add({
+        targets: '.ml6',
+        opacity: 0,
+        duration: 25,
+        easing: "easeOutExpo",
+        delay: 0
+    });
 })
 
 function hideCover() {
@@ -16,4 +25,16 @@ function showContent() {
         $('#show-hide').show();
     }, 50);
 }
+
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  })
 

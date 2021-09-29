134e7fa545a7159bdd3244ec5e854fb4c7f533f8
ccardinale98 <ccardinale98@gmail.com>
2021-09-29 11:00:22 -0400

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
